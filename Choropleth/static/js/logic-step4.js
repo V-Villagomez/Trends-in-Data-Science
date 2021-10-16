988f157c0ce3385e06ede2568f59e14f84d4b7af
Negin Djalali <negindjalali@Negins-MacBook-Air.local>
2021-10-16 11:25:41 -0700

    maps added

--- Choropleth/static/js/logic-step4.js
+++ Choropleth/static/js/logic-step4.js
@@ -1,44 +1,43 @@
-var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5
+
+d3.csv('../data/data_final.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'USA-states',
+          locations: unpack(rows, 'state'),
+          z: unpack(rows, 'avg_Salary'),
+          text: unpack(rows, 'state'),
+          zmin: 0,
+          zmax: 170000,
+          colorscale: [
+              [0, 'rgb(242,240,247)'], [10, 'rgb(218,218,235)'],
+              [10, 'rgb(188,189,220)'], [15, 'rgb(158,154,200)'],
+              [15, 'rgb(117,107,177)'], [20, 'rgb(84,39,143)']
+          ],
+          colorbar: {
+              title: 'Average Salary',
+              thickness: 1.0
+          },
+          marker: {
+              line:{
+                  color: 'rgb(255,255,255)',
+                  width: 2
+              }
+          }
+      }];
+
+
+      var layout = {
+          title: 'Data Science Jobs in USA',
+          geo:{
+              scope: 'usa',
+              showlakes: true,
+              lakecolor: 'rgb(255,255,255)'
+          }
+      };
+
+      Plotly.newPlot("myDiv", data, layout, {showLink: false});
 });
-
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-// var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
-
-// d3.json(url).then(function(response) {
-
-//   console.log(response);
-
-//   var heatArray = [];
-
-//   for (var i = 0; i < response.length; i++) {
-//     var location = response[i].location;
-
-//     if (location) {
-//       heatArray.push([location.coordinates[1], location.coordinates[0]]);
-//     }
-//   }
-
-//   var heat = L.heatLayer(heatArray, {
-//     radius: 20,
-//     blur: 35
-//   }).addTo(myMap);
-
-// });
-d3.csv("../data/data_final.csv").then((feature) => {
-  console.log(feature);
-
-  var heatArray =[];
-  for (var i=0; i< feature.length; i++){
-    heatArray.push([feature[i].latitude, feature[i].longitude]);
-  }
-    var heat = L.heatLayer(heatArray, {
-      rdius: 20,
-      blur: 35
-  }).addTo(myMap);
-  });
