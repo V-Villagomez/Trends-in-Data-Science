a538c7a5141445584b644ff0bf0856c44e8a9ed7
Vanessa Villagomez <villagomez.v@outlook.com>
2021-10-17 23:29:43 -0700

    add updated files

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -10,6 +10,10 @@ from flask import (
 import pandas as pd
 from sqlalchemy import create_engine
 
+# Reference SQL Database
+#engine = create_engine(f'postgresql://dauzivavcrjsot:2eb6527fd42b15514d612ed150b329e9601d0d8fbad490d583edc21e8365d877@ec2-3-226-165-74.compute-1.amazonaws.com:5432/dfh9j8gfut2e72')
+#connection = engine.connect()
+
 # Create an app
 app = Flask(__name__)
 
