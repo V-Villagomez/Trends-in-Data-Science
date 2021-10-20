22aa13a89954dbb24e32081a0408e3fe406213da
Vanessa Villagomez <villagomez.v@outlook.com>
2021-10-19 18:54:23 -0700

    update files

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -10,18 +10,18 @@ from flask import (
 import pandas as pd
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
-#from config import db_url
+from config import db_url
 
 # Create an app
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-#app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 # Define a base "/" route with the @app.route() decorator
-#@app.route("/")
-#def home():
-    #return render_template("index.html")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 # Define what to do when a user hits the /dashboard route
 #app.route("/dashboard")
@@ -32,18 +32,8 @@ app = Flask(__name__)
 #def dashboarddata():
     #return""
 
-# Define what to do when a user hits the /racingchart route
-#app.route("/racingchart")
-#def racingchart():
-    #return""
-
-#app.route("/rachingchartdata")
-#def racingchartdata():
-    #return""
-
-
 # Run the server
-#if __name__ == "__main__":
-    #app.run()
+if __name__ == "__main__":
+    app.run()
     #or, which one would I need?
     #app.run(debug=True)
